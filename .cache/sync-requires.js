375baf8e199e0f817c2bdd5c1bb943096e40ae32
Joshua Stamps <stampsmed91@gmail.com>
2021-02-12 19:32:10 -0600

    logo size reduced

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,7 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": (preferDefault(require("C:\\Users\\Joshua Stamps\\OneDrive\\GitHubRepos\\master-jstamps.dev\\.cache\\dev-404-page.js"))),
+  "component---cache-caches-gatsby-plugin-offline-app-shell-js": (preferDefault(require("C:\\Users\\Joshua Stamps\\OneDrive\\GitHubRepos\\master-jstamps.dev\\.cache\\caches\\gatsby-plugin-offline\\app-shell.js"))),
   "component---src-pages-404-tsx": (preferDefault(require("C:\\Users\\Joshua Stamps\\OneDrive\\GitHubRepos\\master-jstamps.dev\\src\\pages\\404.tsx"))),
   "component---src-pages-index-tsx": (preferDefault(require("C:\\Users\\Joshua Stamps\\OneDrive\\GitHubRepos\\master-jstamps.dev\\src\\pages\\index.tsx"))),
   "component---src-pages-portfolio-tsx": (preferDefault(require("C:\\Users\\Joshua Stamps\\OneDrive\\GitHubRepos\\master-jstamps.dev\\src\\pages\\portfolio.tsx"))),
